b6e716e2bee37c7336e28519f0e375b510b99fee
Surya Kiran <63420304+Surya-827@users.noreply.github.com>
2022-07-09 17:07:27 +0530

    Uploaded solution for coin state

---
+++ CoinState.py
@@ -0,0 +1,28 @@
+a=input()
+
+b=int(input())
+
+if a=="T":
+
+      if b%2==0:
+
+            print("T")
+
+      else:
+
+            print("H")
+
+//allcoding1
+
+else:
+
+       if b%2==0:
+
+             print("H")
+
+       else:
+
+              print("T")
+
+Python3
+
