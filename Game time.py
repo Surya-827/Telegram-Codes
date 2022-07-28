2ec08af9cef9fc1f2b4ddaaceec61eca8d52890f
Surya Kiran <63420304+Surya-827@users.noreply.github.com>
2022-07-28 21:19:45 +0530

    Uploaded solution for Game time code

---
+++ Game time.py
@@ -0,0 +1,19 @@
+def playGame(stars):
+
+    p1=0
+
+    p2=stars.count(1)-stars.count(0)
+
+    c=0
+
+    while p1<p2:
+
+        p1+=1
+
+        p2-=1
+
+        c+=1
+
+    return c
+
+GAME TIME CODE . python 3
