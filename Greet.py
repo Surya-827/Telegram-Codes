3747cef2d5b6f4a9ebf0dcaf25cb72114f009975
Surya-827 <vsuryakiran827@gmail.com>
2022-11-14 02:46:27 +0530

    Added ConcatNames method in Greet.py

--- Greet.py
+++ Greet.py
@@ -5,8 +5,13 @@ class UserProfile(object):
     def greet(cls,name:str) -> str:
         return f"Hello! {name},Welcome Back to Code..."
 
+    @classmethod
+    def ConcatNames(cls,lname:str,fname:str) -> str:
+        return "Full Name is  : "+fname+" "+lname
+
 
 if __name__=="__main__":
     #x = input("Enter User Name : ")
     obj = UserProfile()
-    print(obj.greet("Surya"))
+    #print(obj.greet("Surya"))
+    print(obj.ConcatNames("Kiran","Surya"))
